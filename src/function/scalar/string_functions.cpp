78ee4e524800c86d9af74e546e39f04c40defa32
Christiaan Herrewijn <christiaan@duckdblabs.com>
2024-08-15 08:54:22 +0200

    restructure functions concat and list_concat

--- src/function/scalar/string_functions.cpp
+++ src/function/scalar/string_functions.cpp
@@ -4,7 +4,6 @@ namespace duckdb {
 
 void BuiltinFunctions::RegisterStringFunctions() {
 	Register<StripAccentsFun>();
-	Register<ConcatFun>();
 	Register<ContainsFun>();
 	Register<LengthFun>();
 	Register<LikeFun>();
